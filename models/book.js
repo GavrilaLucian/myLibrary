84f3d38b1c43489f44b51dffabe7f85212be2311
Gavrila Lucian <gavrilalucian34@yahoo.com>
2020-09-24 16:51:25 +0300

    FInished backend3

--- models/book.js
+++ models/book.js
@@ -1,45 +1,45 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
-
-const bookSchema= new mongoose.Schema({
-    title: {
-        type:String,
-        required:true
-    },
-    description: {
-        type:String,
-    },
-    publishDate:{
-        type: Date,
-        require:true
-    },
-    pageCount:{
-        type:Number,
-        require:true
-    },
-    createdAt:{
-        type:Date,
-        require:true,
-        default:Date.now
-    },
-    coverImage:{
-        type:Buffer,
-        require:true
-    },
-    coverImageType:{
-        type:String,
-        required:true
-    },
-    author:{
-        type:mongoose.Schema.Types.ObjectId,
-        require:true,
-        ref:"Author"
-    }
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  publishDate: {
+    type: Date,
+    required: true
+  },
+  pageCount: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  coverImage: {
+    type: Buffer,
+    required: true
+  },
+  coverImageType: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Author'
+  }
 })
 
-bookSchema.virtual('coverImagePath').get(function(){
-    if(this.coverImage !=null&& this.coverImageType!= null){
-      return `data:$(this.coverImageType);charset=utf-8;base64,${this.coverImage.toString(`base64`)}`
-    }
+bookSchema.virtual('coverImagePath').get(function() {
+  if (this.coverImage != null && this.coverImageType != null) {
+    return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
+  }
 })
-module.exports=mongoose.model("Book",bookSchema)
+
+module.exports = mongoose.model('Book', bookSchema)
